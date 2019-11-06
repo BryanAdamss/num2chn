cf336f9258db24f40027ae7877cedeb93484dde4
BryanAdamss <BryanAdamss@foxmail.com>
2019-11-06 15:24:36 +0800

    feat(project): 实现逻辑

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Options {
+  unitChars?: string[]
+  sectionUnitChars?: string[]
+  numChars?: string[]
+  dotChar?: string
+  signChar?: string
+}
+
+export interface NumberParts {
+  sign: string
+  interger: string
+  dot: string
+  decimal: string
+}
